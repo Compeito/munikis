1f70ae8774fb183ce637013a62b551ecb8033d55
Compeito <com0806peito@icloud.com>
2020-03-09 16:12:39 +0900

    feat: 公式アカウント用RTコマンド追加

---
+++ web/core/management/commands/retweet.py
@@ -0,0 +1,42 @@
+from time import sleep
+
+from django.core.management.base import BaseCommand
+from account.models import User
+
+
+class Command(BaseCommand):
+    help = ''
+
+    def handle(self, *args, **options):
+        try:
+            tsukuriga_user = User.objects.get(username='tsukuriga')
+        except:
+            return
+        if tsukuriga_user.has_twitter_auth:
+            terms = (
+                '(',
+                '("ツクリガ" filter:links)',
+                'OR'
+                '#tsukuriga',
+                'OR'
+                '(source:"tsukuriga" card_name:animated_gif)',
+                ')',
+                '-from:tsukuriga',
+                '-from:ugo_compeito',
+                '-filter:retweets'
+            )
+            tweets = tsukuriga_user.api.GetSearch(term=' '.join(terms), count=50)
+            count = 0
+            for tweet in tweets[::-1]:
+                if count >= 5:
+                    break
+                if not tweet.retweeted:
+                    try:
+                        tsukuriga_user.api.PostRetweet(tweet.id)
+                        print(f'RT: https://twitter.com/_/status/{tweet.id}')
+                        count += 1
+                        sleep(5 * 60)
+                    except KeyboardInterrupt:
+                        break
+                    except:
+                        pass

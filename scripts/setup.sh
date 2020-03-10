9ab62016c92ee96481481e565daddee40bb43f4f
Compeito <com0806peito@icloud.com>
2020-03-10 23:39:03 +0900

    feat: nodeが立ち上がらないよう変更

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,18 +3,19 @@
 cd `dirname $0`/../
 
 docker-compose down && docker-compose build web
+docker-compose up --no-deps -d db
 
 echo "パッケージのインストール"
-docker-compose run web poetry install --no-root
+docker-compose run --no-deps web poetry install --no-root
 echo "データベースのマイグレーション"
-docker-compose run web python manage.py migrate
+docker-compose run --no-deps web python manage.py migrate
 echo "データベースの初期化"
-docker-compose run web python manage.py flush --no-input
+docker-compose run --no-deps web python manage.py flush --no-input
 echo "mediaディレクトリ下を削除"
 rm -rf web/media/*
 echo "初期データ作成"
-docker-compose run web python manage.py seed
+docker-compose run --no-deps web python manage.py seed
 echo "ランキング作成"
-docker-compose run web python manage.py ranking
+docker-compose run --no-deps web python manage.py ranking
 
 docker-compose down && docker-compose up -d

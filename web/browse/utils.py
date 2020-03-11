641b1ee30aae37f5f3e013fe9dc6fd6da80daa2b
Compeito <com0806peito@icloud.com>
2020-03-11 14:40:38 +0900

    perf: safe_videosのN+1改善
    refs #111

--- web/browse/utils.py
+++ web/browse/utils.py
@@ -2,9 +2,13 @@ from upload.models import Video
 
 
 def safe_videos():
-    return Video.objects.filter(
-        is_ban=False,
-        profile__release_type='published',
-        profile__isnull=False,
-        data__isnull=False
+    return (
+        Video.objects
+            .prefetch_related('user', 'profile', 'data', 'point_set', 'favorite_set')
+            .filter(
+                is_ban=False,
+                profile__release_type='published',
+                profile__isnull=False,
+                data__isnull=False
+            )
     )

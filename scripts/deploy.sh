f20d63f256fb84e32283bc4275f6703d8dbcc057
Compeito <com0806peito@icloud.com>
2020-03-12 22:02:48 +0900

    feat: デプロイスクリプト調整

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,18 +4,18 @@ set -e
 cd `dirname $0`/../
 
 alias docker-compose-prod='docker-compose -f docker-compose.yml -f docker-compose.prod.yml'
-docker-compose-prod down
 
 # イメージの再ビルド
 docker-compose-prod build
 
 # 依存関係の更新とDBマイグレーション
-docker-compose-prod run web bash -c "poetry install --no-dev --no-root && python manage.py migrate"
+docker-compose-prod up --no-deps -d db
+docker-compose-prod run --no-deps --rm web python manage.py migrate
 
 # js/cssの再ビルドと配置
 rm -rf nginx/assets/bundles/*
 rm -rf nginx/assets/webpack-stats.json
-docker-compose-prod run node bash -c "yarn && yarn run build"
-docker-compose-prod run web python manage.py collectstatic --no-input
+docker-compose-prod run --no-deps --rm node bash -c "yarn && yarn run build"
+docker-compose-prod run --no-deps --rm web python manage.py collectstatic --no-input
 
-docker-compose-prod up -d
+docker-compose-prod up -d --force-recreate

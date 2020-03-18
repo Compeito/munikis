246639c746bb631b5893f605edf42279cd3ede0b
mkizka <30231179+mkizka@users.noreply.github.com>
2020-03-19 00:02:22 +0900

    feat: 共通処理をUserMixinとして分離

--- web/account/users/views.py
+++ web/account/users/views.py
@@ -1,106 +1,87 @@
 from django.shortcuts import get_object_or_404
 
 from ..models import User
-from ajax.models import Favorite
 from browse.utils import safe_videos, unsafe_videos
 from core.utils import AltPaginationListView
 
 
-def get_tabs(n, account: User):
-    tabs = [
-        {'href': f'/u/{account.username}', 'title': '投稿動画', 'is_active': False},
-        {'href': f'/u/{account.username}/favorites', 'title': 'お気に入りリスト', 'is_active': False},
-        {'href': f'/u/{account.username}/followees', 'title': f'{account.followees.count()}フォロイー', 'is_active': False},
-        {'href': f'/u/{account.username}/followers', 'title': f'{account.followers.count()}フォロワー', 'is_active': False},
-    ]
-    tabs[n]['is_active'] = True
-    return tabs
+class UserMixin:
+    account: User = None
+    tab_index = 0
 
+    def get(self, request, *args, **kwargs):
+        self.account = get_object_or_404(User, username=self.kwargs['username'])
+        return super().get(request, *args, **kwargs)
 
-class Profile(AltPaginationListView):
-    template_name = 'users/profile.html'
-    context_object_name = 'videos'
-    paginate_by = 12
-
-    def get_queryset(self):
-        username = self.kwargs['username']
-        if username == self.request.user.username or self.request.user.is_staff:
-            return unsafe_videos().filter(user__username=username).order_by('-profile__created_at')
-        return safe_videos().filter(user__username=username).order_by('-profile__created_at')
+    def get_tabs(self):
+        account = self.account
+        tabs = [
+            {'href': f'/u/{account.username}', 'title': '投稿動画', 'is_active': False},
+            {'href': f'/u/{account.username}/favorites', 'title': 'お気に入りリスト', 'is_active': False},
+            {'href': f'/u/{account.username}/followees', 'title': f'{account.followees.count()}フォロイー',
+             'is_active': False},
+            {'href': f'/u/{account.username}/followers', 'title': f'{account.followers.count()}フォロワー',
+             'is_active': False},
+        ]
+        tabs[self.tab_index]['is_active'] = True
+        return tabs
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-
-        account = get_object_or_404(User, username=self.kwargs['username'])
-        context['account'] = account
-        context['tabs'] = get_tabs(0, account)
-
+        context['account'] = self.account
+        context['tabs'] = self.get_tabs()
         return context
+
+
+class Profile(UserMixin, AltPaginationListView):
+    template_name = 'users/profile.html'
+    context_object_name = 'videos'
+    paginate_by = 12
+    tab_index = 0
+
+    def get_queryset(self):
+        if self.account == self.request.user or self.request.user.is_staff:
+            return unsafe_videos().filter(user=self.account).order_by('-profile__created_at')
+        return safe_videos().filter(user=self.account).order_by('-profile__created_at')
 
 
 profile = Profile.as_view()
 
 
-class FavoritesList(AltPaginationListView):
+class FavoritesList(UserMixin, AltPaginationListView):
     template_name = 'users/favorites.html'
     context_object_name = 'videos'
     paginate_by = 12
+    tab_index = 1
 
     def get_queryset(self):
-        username = self.kwargs['username']
-        return safe_videos().filter(favorite__user__username=username).order_by('-favorite__created_at')
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-
-        account = get_object_or_404(User, username=self.kwargs['username'])
-        context['account'] = account
-        context['tabs'] = get_tabs(1, account)
-
-        return context
+        return safe_videos().filter(favorite__user=self.account).order_by('-favorite__created_at')
 
 
 favorites_list = FavoritesList.as_view()
 
 
-class FolloweesList(AltPaginationListView):
+class FolloweesList(UserMixin, AltPaginationListView):
     template_name = 'users/followees.html'
     context_object_name = 'accounts'
     paginate_by = 9
+    tab_index = 2
 
     def get_queryset(self):
-        username = self.kwargs['username']
-        return User.objects.filter(follower_friendships__user__username=username)
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-
-        account = get_object_or_404(User, username=self.kwargs['username'])
-        context['account'] = account
-        context['tabs'] = get_tabs(2, account)
-
-        return context
+        return User.objects.filter(follower_friendships__user=self.account)
 
 
 followees_list = FolloweesList.as_view()
 
-class FollowersList(AltPaginationListView):
+
+class FollowersList(UserMixin, AltPaginationListView):
     template_name = 'users/followers.html'
     context_object_name = 'accounts'
     paginate_by = 9
+    tab_index = 3
 
     def get_queryset(self):
-        username = self.kwargs['username']
-        return User.objects.filter(followee_friendships__followee__username=username)
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-
-        account = get_object_or_404(User, username=self.kwargs['username'])
-        context['account'] = account
-        context['tabs'] = get_tabs(3, account)
-
-        return context
+        return User.objects.filter(followee_friendships__followee=self.account)
 
 
 followers_list = FollowersList.as_view()

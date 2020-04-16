2c13fa9e8465e9b74620e4681e7212a4191ccb14
mkizka <30231179+mkizka@users.noreply.github.com>
2020-04-16 20:04:22 +0900

    fix: ssh後のコマンド修正

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-set -e
-cd `dirname $0`/../
-
-cd tsukuriga
-git fetch origin
-git reset --hard origin/migration/gcp-run
-
 CRON="
 ENTRYPOINT=\"cd ~/tsukuriga && docker-compose run --rm -d web poetry run\"
 * * * * *   \$EXTRYPOINT encode
@@ -16,6 +9,7 @@ ENTRYPOINT=\"cd ~/tsukuriga && docker-compose run --rm -d web poetry run\"
 0 0 * * *   \$EXTRYPOINT ping_google
 30 18 * * * \$EXTRYPOINT retweet
 "
+
 FILEPATH=/var/tmp/cron-$(date +\%Y\%m\%d-\%H\%M\%S).conf
 echo "$CRON" > $FILEPATH
 crontab $FILEPATH

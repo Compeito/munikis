85324d6ccbd92d581c277b7fbcbfb6cd8e61c6cf
mkizka <30231179+mkizka@users.noreply.github.com>
2020-04-18 00:00:16 +0900

    feat: processの数を4に指定

--- web/start.sh
+++ web/start.sh
@@ -10,5 +10,10 @@ python manage.py migrate
 if [ $DEBUG = "true" ]; then
   python manage.py runserver_plus 0.0.0.0:8080
 else
-  uwsgi --http-socket 0.0.0.0:$HTTP_PORT --module tsukuriga.wsgi
+  uwsgi \
+    --http-socket 0.0.0.0:$HTTP_PORT \
+    --master \
+    --processes 4 \
+    --threads 4 \
+    --module tsukuriga.wsgi
 fi

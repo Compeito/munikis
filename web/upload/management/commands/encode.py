c7a4a6476997685c30c51dc0dbde97902b613c03
mkizka <30231179+mkizka@users.noreply.github.com>
2020-04-20 16:39:20 +0900

    fix: エンコードとgif作成処理を一つずつに

--- web/upload/management/commands/encode.py
+++ web/upload/management/commands/encode.py
@@ -1,46 +1,30 @@
 import traceback
-from multiprocessing import Process
 
 from django.conf import settings
 from django.core.mail import send_mail
+from upload.models import UploadedPureVideo
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        # ファイルトップでimportするとAppRegistryNotReadyエラーが出る(encode()内のimportが無視される？)
-        from upload.models import UploadedPureVideo
-        pure_videos = UploadedPureVideo.objects.all().order_by('-created_at')
+        pure = UploadedPureVideo.objects.first()
+        if pure.is_encoding:
+            return
 
-        for pure in pure_videos:
-            # エラーを送出してもメインプロセスは終了しないため、try-exceptはtarget内部で行う
-            process = Process(target=encode, args=[pure.id])
-            process.start()
-            process.join()
+        try:
+            pure.make()
+            pure.delete()
+        except:
+            pure.is_failed = True
+            pure.traceback = traceback.format_exc()
+            pure.save()
 
-
-def encode(pk):
-    import django
-    django.setup()
-    from upload.models import UploadedPureVideo
-
-    pure = UploadedPureVideo.objects.get(pk=pk)
-    if pure.is_encoding:
-        return
-
-    try:
-        pure.make()
-        pure.delete()
-    except:
-        pure.is_failed = True
-        pure.traceback = traceback.format_exc()
-        pure.save()
-
-        if not settings.DEBUG:
-            send_mail(
-                subject='エンコード中のエラー通知',
-                message=f'{pure.traceback}\nhttps://tsukuriga.net/admin/upload/uploadedpurevideo/{pure.id}/change/',
-                from_email=settings.SERVER_EMAIL,
-                recipient_list=[email for name, email in settings.ADMINS]
-            )
+            if not settings.DEBUG:
+                send_mail(
+                    subject='エンコード中のエラー通知',
+                    message=f'{pure.traceback}\nhttps://tsukuriga.net/admin/upload/uploadedpurevideo/{pure.id}/change/',
+                    from_email=settings.SERVER_EMAIL,
+                    recipient_list=[email for name, email in settings.ADMINS]
+                )
--- web/upload/management/commands/gif.py
+++ web/upload/management/commands/gif.py
@@ -6,5 +6,5 @@ from upload.models import VideoData
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for video in VideoData.objects.filter(Q(gif__isnull=True) | Q(gif__exact=''), duration__gte=3):
-            video.update_gif()
+        video = VideoData.objects.filter(Q(gif__isnull=True) | Q(gif__exact=''), duration__gte=3).first()
+        video.update_gif()

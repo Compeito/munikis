186c208a18207d9accbb170c78b83118032a9d22
mkizka <30231179+mkizka@users.noreply.github.com>
2020-04-21 16:46:46 +0900

    feat: uwsgiのオプションをiniに分離してlimit-postを200MBに

--- web/start.sh
+++ web/start.sh
@@ -10,10 +10,5 @@ python manage.py migrate
 if [ $DEBUG = "true" ]; then
   python manage.py runserver_plus 0.0.0.0:8080
 else
-  uwsgi \
-    --http-socket 0.0.0.0:$HTTP_PORT \
-    --master \
-    --processes 4 \
-    --threads 4 \
-    --module tsukuriga.wsgi
+  uwsgi --http-socket 0.0.0.0:$HTTP_PORT --ini uwsgi.ini
 fi

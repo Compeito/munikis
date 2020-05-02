b83c745f50664dc206bf75e95711d05cb1aa2b56
mkizka <30231179+mkizka@users.noreply.github.com>
2020-05-02 18:21:53 +0900

    feat: migrateが本番環境でのみ実行されるように

--- web/start.sh
+++ web/start.sh
@@ -5,7 +5,11 @@ if [ -n "$PORT" ]; then
   HTTP_PORT=$PORT
 fi
 
-python manage.py migrate
+# Cloud Runが定義する環境変数で本番環境を判定
+# https://cloud.google.com/run/docs/reference/container-contract?hl=ja#env-vars
+if [ -n "$K_SERVICE" ]; then
+  python manage.py migrate
+fi
 
 if [ $DEBUG = "true" ]; then
   python manage.py runserver_plus 0.0.0.0:8080

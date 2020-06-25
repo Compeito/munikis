cea0cd7acbd9a8e02f88a0a77cb020c4334dc7f0
mkizka <30231179+mkizka@users.noreply.github.com>
2020-06-25 17:25:29 +0900

    feat: 変更できる再生スピードを制限

--- node/src/core/watch/player-setup.js
+++ node/src/core/watch/player-setup.js
@@ -7,5 +7,9 @@ ready(() => {
   const player = new Plyr('#video-player', {
     ratio: '16:9',
     keyboard: {focused: false, global: false},
+    speed: {
+      selected: 1,
+      options: [0.5, 0.75, 1]
+    }
   })
 })
